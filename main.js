70324b27501357385ebeacbcffcc6c89a6cbe999
James W <james.wu@alumni.olin.edu>
2017-04-04 23:52:51 -0400

    update to electron 1.0 conventions

--- main.js
+++ main.js
@@ -3,6 +3,9 @@
 const electron = require('electron');
 const {app, BrowserWindow} = electron;
 
+const path = require('path');
+const url = require('url');
+
 let mainWindow;
 
 app.on('ready', function() {
@@ -11,5 +14,32 @@ app.on('ready', function() {
         width: 800
     });
 
-    mainWindow.loadURL('file://' + __dirname + '/app/index.html');
+    mainWindow.loadURL(url.format({
+        pathname: path.join(__dirname, 'app/index.html'),
+        protocol: 'file:',
+        slashes: true
+    }));
+
+    mainWindow.on('closed', function () {
+        // Dereference the window object, usually you would store windows
+        // in an array if your app supports multi windows, this is the time
+        // when you should delete the corresponding element.
+        mainWindow = null;
+    })
+});
+
+app.on('window-all-closed', function () {
+    // On OS X it is common for applications and their menu bar
+    // to stay active until the user quits explicitly with Cmd + Q
+    if (process.platform !== 'darwin') {
+        app.quit();
+    }
+});
+
+app.on('activate', function () {
+    // On OS X it's common to re-create a window in the app when the
+    // dock icon is clicked and there are no other windows open.
+    if (mainWindow === null) {
+        createWindow();
+    }
 });
